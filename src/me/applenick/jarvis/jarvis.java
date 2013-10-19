211c620272929a0ba8e01c4b2c3d059af19cddd2
applenick <applenick3@me.com>
2013-10-19 01:24:30 -0700

    small stuff

--- src/me/applenick/jarvis/jarvis.java
+++ src/me/applenick/jarvis/jarvis.java
@@ -1,29 +1,47 @@
 package me.applenick.jarvis;
 
+import me.applenick.jarvis.events.Events;
+
 import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class jarvis extends JavaPlugin {
 	
 	public static String user = "";
 	public static String nick = "";
-	public static String Pin = "" ;  
+	public static String Pin = "" ; 
+	public static String motd = "";
 
 	
 	public void onEnable(){
+		//import config
 		getConfig().options().copyDefaults();
 		saveConfig();
 		user = MSG(getConfig().getString("userFull"));
 		nick = MSG(getConfig().getString("userNick"));
 		Pin = MSG(getConfig().getString("Pin"));
+		motd = MSG(getConfig().getString("motd"));
+
+		
+		//events
+		getServer().getPluginManager()
+		.registerEvents(new Events(), this);
 	}
 	
 	public void onDisable(){
 		saveConfig();
 		
 	}
+	
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLable, String[] args){
+		if(cmd.getName().equalsIgnoreCase("jarvis")){
+			
+		}
+	}
+	
 	
 	
     public String MSG(String Message) {
