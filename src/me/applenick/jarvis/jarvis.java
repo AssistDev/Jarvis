9e51e44da633ff14bf25fe3965fbcf4d47bc5ea5
applenick <applenick3@me.com>
2013-10-19 00:43:33 -0700

    Start

---
+++ src/me/applenick/jarvis/jarvis.java
@@ -0,0 +1,18 @@
+package me.applenick.jarvis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class jarvis extends JavaPlugin {
+
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+
+	w
+	}
+	
+}
