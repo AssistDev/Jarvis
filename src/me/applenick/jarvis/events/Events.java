5845d466ed525fd926e49cfd7b3bd7e85ced4056
applenick <applenick3@me.com>
2013-10-19 01:24:23 -0700

    Started work on join event

--- src/me/applenick/jarvis/events/Events.java
+++ src/me/applenick/jarvis/events/Events.java
@@ -1,5 +1,43 @@
 package me.applenick.jarvis.events;
 
-public class Events {
+import me.applenick.jarvis.jarvis;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class Events implements Listener {
+	
+	private String user = jarvis.user;
+	private String nick = jarvis.nick;
+	private String pin = jarvis.Pin;
+	private String motd = jarvis.motd;
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event){
+		if(!(event.getPlayer().getName().equalsIgnoreCase(user))){
+			event.getPlayer().sendMessage(motd);
+			return;
+		}
+		Player p = event.getPlayer();
+		p.sendMessage(ChatColor.AQUA + "Welcome Back Sir,");
+		p.sendMessage("I am going to need your Pin for you to access me.");
+		p.sendMessage("Use /login (pin) to proceed.");
+		return;
+	}
+	
+	@EventHandler 
+	public void onLeave(PlayerQuitEvent event){
+		
+	}
+	
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event){
+		
+	}
 
 }
