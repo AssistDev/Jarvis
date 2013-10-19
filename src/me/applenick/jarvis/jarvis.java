98501d426f0b7a2f2214adf4a7cfeccaba0a996a
applenick <applenick3@me.com>
2013-10-19 01:08:36 -0700

    add config stuff + messages

--- src/me/applenick/jarvis/jarvis.java
+++ src/me/applenick/jarvis/jarvis.java
@@ -1,16 +1,32 @@
 package me.applenick.jarvis;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class jarvis extends JavaPlugin {
+	
+	public static String user = "";
+	public static String nick = "";
+	public static String Pin = "" ;  
 
 	
 	public void onEnable(){
-		
+		getConfig().options().copyDefaults();
+		saveConfig();
+		user = MSG(getConfig().getString("userFull"));
+		nick = MSG(getConfig().getString("userNick"));
+		Pin = MSG(getConfig().getString("Pin"));
 	}
 	
 	public void onDisable(){
+		saveConfig();
 		
 	}
 	
+	
+    public String MSG(String Message) {
+        return Message.replaceAll("~([a-z0-9])", ChatColor.COLOR_CHAR + "$1");
+    }
+    
+    
 }
